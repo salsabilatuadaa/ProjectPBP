5d4182fe6a49320d2e25ea86ea08e9ee811a6e9f
nurannisaamf <nurannisaameiarti@gmail.com>
2023-12-17 07:46:22 +0700

    coba UI create transaksi

---
+++ app/src/main/java/com/android/projectpbp/CreateTransactionActivity.java
@@ -0,0 +1,12 @@
+package com.android.projectpbp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CreateTransactionActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_transaksi);
+    }
+}
